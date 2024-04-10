f36bc2fe15887ad4c70c7b50139e191ac59d4b21
Joren Six <joren.six@ugent.be>
2024-04-10 14:15:27 +0200

    Main docker config

---
+++ init_dev.sh
@@ -0,0 +1,15 @@
+# Backend
+git clone git@github.com:GhentCDH/mela_backend.git backend
+
+# Frontend
+git clone git@github.com:GhentCDH/mela_frontend.git frontend
+
+# Mounted directories
+mkdir -p ~/.docker/mounts/mela/bakend/homedir
+mkdir -p ~/.docker/mounts/mela/frontend/homedir
+
+mkdir -p ~/.docker/mounts/mela/elasticsearch
+
+mkdir -p ~/.docker/mounts/mela/data/app
+mkdir -p ~/.docker/mounts/mela/data/test
+mkdir -p ~/.docker/mounts/mela/data/auth
