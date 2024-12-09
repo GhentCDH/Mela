0af65a226fefd022078cf7543d9fd0c3475df09b
Bo Vandersteene <bo.vandersteene@firepeak.tech>
2024-12-09 15:08:27 +0100

    feat: add backend health check

--- libs/tools/health/api/src/lib/health-api.module.ts
+++ libs/tools/health/api/src/lib/health-api.module.ts
@@ -1,9 +1,15 @@
 import {Module} from '@nestjs/common';
 import {TerminusModule} from "@nestjs/terminus";
 import {HealthController} from "./health.controller";
+import {HttpModule} from "@nestjs/axios";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-    imports: [TerminusModule],
+    imports: [TerminusModule,
+        HttpModule,
+        ConfigModule
+        // PrismaModule
+    ],
     controllers: [HealthController],
     providers: [],
     exports: [],
