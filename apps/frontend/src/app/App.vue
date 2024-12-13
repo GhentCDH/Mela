e4aa0e817a8ce03a55ae60ff8de722d2b6f293b5
Bo Vandersteene <1673799+bovandersteene@users.noreply.github.com>
2024-12-13 09:24:41 +0100

    feat: add tailwind (#12)
    * feat: add tailwind
    
    * feat: add tailwind
    
    * feat: add tailwind
    
    ---------
    
    Co-authored-by: Bo Vandersteene <bo.vandersteene@firepeak.tech>

--- apps/frontend/src/app/App.vue
+++ apps/frontend/src/app/App.vue
@@ -1,30 +1,18 @@
 <script setup lang="ts">
-import {useAuthenticationStore, useHttpStore} from "@ghentcdh/authentication/frontend";
-import {Debugger} from "@ghentcdh/tools/logging/frontend";
+import { useAuthenticationStore } from '@ghentcdh/authentication/frontend';
+import { Debugger } from '@ghentcdh/tools/logging/frontend';
 
+const authenticationStore = useAuthenticationStore();
 
-const authenticationStore = useAuthenticationStore()
-const httpStore = useHttpStore();
-
-Debugger.table(import.meta.env)
-const checkAuth = () => {
-  httpStore.post('/api/auth/login', {}).then(response => {
-    Debugger.log(response);
-    alert('login ok')
-  });
-}
-
-window.setTimeout(() => {
-  checkAuth();
-}, 1000);
+Debugger.table(import.meta.env);
 </script>
 
 <template>
-  {{ authenticationStore.isAuthenticated() }}<br>
-  {{ authenticationStore.token() }}<br>
+  <div class="navbar bg-primary">
+    <a class="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  {{ authenticationStore.isAuthenticated() }}<br />
+  {{ authenticationStore.token() }}<br />
   <pre>{{ authenticationStore.user() }}</pre>
   Hello world
-  <button @click="checkAuth">
-    Check Auth
-  </button>
 </template>
---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+const { createGlobPatternsForDependencies } = require('@nx/vue/tailwind');
+
+const sharedTailwindConfig = require('../../libs/ui/src/tailwind.config');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [sharedTailwindConfig],
+  content: [
+    join(__dirname, 'index.html'),
+    join(__dirname, 'src/**/*!(*.stories|*.spec).{vue,ts,tsx,js,jsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+};
