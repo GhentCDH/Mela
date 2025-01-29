9e4e1cd9f0038d29399e0a43b54a5a6c539b84b2
Bo Vandersteene <1673799+bovandersteene@users.noreply.github.com>
2025-01-29 19:00:51 +0100

    feat: style headings (#47)
    Co-authored-by: Bo Vandersteene <bo.vandersteene@firepeak.tech>

---
+++ libs/ui/src/_scss/style-item.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+type StyleItem = { as?: string; class?: string };
+const props = withDefaults(defineProps<StyleItem>(), {
+  as: 'div',
+  class: '',
+});
+</script>
+
+<template>
+  <as :class="props.class">
+    Lorem ipsum odor amet.
+  </as>
+</template>
