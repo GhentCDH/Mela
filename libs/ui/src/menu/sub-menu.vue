79bab16d578acfd6f6ff2348d12e51fff7d0d786
Bo Vandersteene <bo@firepeak.tech>
2025-03-06 14:46:36 +0100

    feat: add menu component

--- libs/ui/src/menu/sub-menu.vue
+++ libs/ui/src/menu/sub-menu.vue
@@ -6,14 +6,21 @@
       :class="[btnClass, 'btn btn-ghost text-right flex-nowrap flex']"
     >
       <span> {{ label }}</span>
-      <Icon :icon="IconEnum.ChevronDown" :size="Size.sm" />
+      <Icon
+        :icon="IconEnum.ChevronDown"
+        :size="Size.sm"
+      />
     </div>
     <ul
       tabindex="0"
       class="menu dropdown-content bg-white z-1 mt-4 p-2 shadow-sm"
     >
       <li class="min-w-max flex-no-wrap text-black">
-        <MenuItem v-for="item in items" :key="item.label" v-bind="item" />
+        <MenuItem
+          v-for="item in items"
+          :key="item.label"
+          v-bind="item"
+        />
       </li>
     </ul>
   </div>
@@ -22,8 +29,8 @@
 <script setup lang="ts">
 import MenuItem from './menu-item.vue';
 import type { MenuWithItems } from './menu.type';
-import { Icon, IconEnum } from '../icons';
 import { Size } from '../const';
+import { Icon, IconEnum } from '../icons';
 
 const properties = defineProps<MenuWithItems & { btnClass: string }>();
 </script>
--- libs/ui/src/shell/shell.component.vue
+++ libs/ui/src/shell/shell.component.vue
@@ -2,7 +2,11 @@
   <toast />
   <ModalWrapper />
   <div class="drawer">
-    <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+    <input
+      id="my-drawer-3"
+      type="checkbox"
+      class="drawer-toggle"
+    >
     <div class="drawer-content flex flex-col h-[100VH]">
       <nav class="navbar bg-primary text-primary-content w-full">
         <div class="navbar-start">
@@ -15,10 +19,17 @@
           </label>
         </div>
         <div class="navbar-center">
-          <a class="btn btn-ghost text-xl" :href="baseUrl">{{ title }}</a>
+          <a
+            class="btn btn-ghost text-xl"
+            :href="baseUrl"
+          >{{ title }}</a>
         </div>
         <div class="navbar-end">
-          <Submenu v-if="user" v-bind="userMenu" :btn-class="''" />
+          <Submenu
+            v-if="user"
+            v-bind="userMenu"
+            :btn-class="''"
+          />
         </div>
       </nav>
       <div class="p-2 bg-white mb-1 mr-1 flex-1 shadow-sm">
@@ -32,7 +43,10 @@
         class="drawer-overlay"
       />
       <ul class="menu bg-base-200 min-h-full w-80 p-4">
-        <li v-for="item of menu" :key="item.label">
+        <li
+          v-for="item of menu"
+          :key="item.label"
+        >
           <RouterLink :to="{ name: item.routerLink, params: item.params }">
             {{ item.label }}
           </RouterLink>
@@ -43,13 +57,14 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+
 import type { ShellMenu, User } from './menu.type';
+import { Size } from '../const';
 import { Icon, IconEnum } from '../icons';
+import { Submenu } from '../menu';
 import { ModalWrapper } from '../modal';
 import Toast from '../toast/toast.vue';
-import { Size } from '../const';
-import { computed } from 'vue';
-import { Submenu } from '../menu';
 
 const baseUrl = import.meta.env.BASE_URL;
 
