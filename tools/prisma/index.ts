bea669848e3850c335bc37d8d9f5771c756bc129
Bo Vandersteene <bo@firepeak.tech>
2025-03-24 17:24:09 +0100

    feat: improve prisma generation

--- tools/prisma/index.ts
+++ tools/prisma/index.ts
@@ -1,11 +1,16 @@
-import { generatePrismaService } from './prisma';
-import * as path from 'path';
+import { fixPrismaImports } from './fix-imports';
 import { generateForm } from './form';
+import { generatePrismaService } from './prisma';
+import * as model from '../../generated/types/modelSchema';
+import * as path from 'path';
 
 const prismaPackage = `@prisma/client`;
 const generatedDir = path.join(__dirname, '../../generated');
 const prismaDir = path.join(generatedDir, 'prisma');
 const typesDir = path.join(generatedDir, 'types');
+const dtoDir = path.join(generatedDir, 'types/dtos');
+const formDir = path.join(generatedDir, 'types/forms');
 
-generateForm(typesDir);
+fixPrismaImports(typesDir);
+generateForm(dtoDir, formDir, model);
 generatePrismaService(prismaPackage, prismaDir);
