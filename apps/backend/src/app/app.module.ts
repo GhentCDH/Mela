536f1f91edbce95a77fd39d4d8dab50776fca343
Bo Vandersteene <bo@firepeak.tech>
2025-03-25 15:59:29 +0100

    feat: disable all backend requests

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { TextApiModule } from '@mela/text/api';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-
-import { AuthenticationApiModule } from '@ghentcdh/authentication-api';
-import { HealthApiModule } from '@ghentcdh/tools-api';
 
 @Module({
   imports: [
-    AuthenticationApiModule,
-    ConfigModule.forRoot(),
-    HealthApiModule,
-    TextApiModule,
-  ],
+    // AuthenticationApiModule,
+    // ConfigModule.forRoot(),
+    // HealthApiModule,
+    // TextApiModule,
+  ]
 })
-export class AppModule {}
+export class AppModule {
+}
